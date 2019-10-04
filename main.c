e6439b648edddd3dccbb54e86834d4d09522e97d
narayan <narayanspai2000@gmail.com>
2019-10-04 23:57:04 +0530

    fixed bug in parse

--- main.c
+++ main.c
@@ -5,12 +5,14 @@
 // flag=1 when exit statement is encountered
 int flag = 0;
 
-//parses the string into many array of strings seperated by either
-//    1) NULL pointer to indicate semicolans.
-//    2) NULL pointer followed by an array with null character as the first element,to indicate end of line
-//eg:
-//   cat file ; ls -l
-//	 will be parsed into [ ['c','a','t'], ['f', 'i', 'l', 'e'], NULL, ['l', 's'], ['-','l'],  NULL,['\0'] ]
+/*
+parses the string into many array of strings seperated by either
+   1) NULL pointer to indicate semicolans.
+   2) NULL pointer followed by an array with null character as the first element,to indicate end of line
+eg:
+  cat file ; ls -l
+	 will be parsed into [ ['c','a','t'], ['f', 'i', 'l', 'e'], NULL, ['l', 's'], ['-','l'],  NULL,['\0'] ]
+*/
 char **parse(char *line)
 {
 	unsigned int BUFFER_SIZE = 10;
@@ -21,8 +23,7 @@ char **parse(char *line)
 	{
 		printf("\nDynammic allocation failed");
 	}
-
-	while (line[next_index_of_line] != '\0')
+	while (line[next_index_of_line] != 10)
 	{
 		if (curr_arr_size == (arr_size - 1))
 		{
@@ -34,7 +35,7 @@ char **parse(char *line)
 			}
 		}
 
-		// if it is a semicolon, make the next array as 1 as its first element to indicate completion of previous command
+		// if it is a semicolon, next pointer is NULL
 		if (line[next_index_of_line] == ';')
 		{
 			rv[curr_arr_size] = NULL;
@@ -42,21 +43,26 @@ char **parse(char *line)
 			curr_arr_size++;
 			continue;
 		}
-		rv[curr_arr_size] = (char *)malloc(20 * sizeof(char)); //assuming each word doesnt exceed 20 chars
+
+		//if a word
 		int curr_word_index = 0;
-		while (line[next_index_of_line] != ';' && line[next_index_of_line] != ' ' && line[next_index_of_line] != '\0')
+		if ((line[next_index_of_line] != ' ') && (line[next_index_of_line] != 10))
 		{
-			rv[curr_arr_size][curr_word_index] = line[next_index_of_line];
-			curr_word_index++;
-			next_index_of_line++;
+			rv[curr_arr_size] = (char *)malloc(20 * sizeof(char)); //assuming each word doesnt exceed 20 chars
+			while (line[next_index_of_line] != ';' && line[next_index_of_line] != ' ' && line[next_index_of_line] != 10)
+			{
+				rv[curr_arr_size][curr_word_index] = line[next_index_of_line];
+				curr_word_index++;
+				next_index_of_line++;
+			}
+			rv[curr_arr_size][curr_word_index] = '\0';
+			curr_arr_size++;
 		}
 
-		rv[curr_arr_size][curr_word_index] = '\0';
+		//if a space
 		if (line[next_index_of_line] == ' ')
 			while (line[++next_index_of_line] == ' ')
 				;
-
-		curr_arr_size++;
 	}
 	rv[curr_arr_size] = NULL;
 	curr_arr_size++;
@@ -94,6 +100,8 @@ void execute(char **pointer_to_str_arr)
 		else if (pointer_to_str_arr[i][0] != '\0')
 		{
 			// execvp(pointer_to_str_arr[i], &(pointer_to_str_arr[i]))
+			while (pointer_to_str_arr[++i] != NULL)
+				;
 		}
 
 		// for NULLs
@@ -112,7 +120,7 @@ void loop()
 	{
 		printf("\nxD>>");
 
-		unsigned int LINE_BUFFER_SIZE = 10;
+		long unsigned int LINE_BUFFER_SIZE = 10;
 		line = (char *)malloc(LINE_BUFFER_SIZE * sizeof(char));
 		bytes_read = getline(&line, &LINE_BUFFER_SIZE, stdin);
 		// 'line' will now be pointing to the first character of the string
@@ -129,8 +137,19 @@ void loop()
 			continue;
 		}
 		int i = 0;
+
 		// pointer_to_str_arr points to an array where each element points to the start of a word
 		pointer_to_str_arr = parse(line);
+		while (i < 12)
+		{
+			if (pointer_to_str_arr[i] == NULL)
+				printf("NULL\n");
+			else
+			{
+				printf("%s\n", pointer_to_str_arr[i]);
+			}
+			i++;
+		}
 		execute(pointer_to_str_arr);
 	}
 }
